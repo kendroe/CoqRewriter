617d906aae5c496d55b1b93899ca8805663cf35c
Kenneth Roe <kendroe@Kenneths-MBP.home>
2017-10-07 11:05:25 -0400

    Added GNU 3.0 public license

--- plugin/src/match.mli
+++ plugin/src/match.mli
@@ -6,7 +6,18 @@
  *                               
  * (C) 2017, Kenneth Roe  
  *  
- * All rights reserved--This is an incomplete work.  An appropriate license
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * will be provided when the work is complete.
  *          
  * For a commercial license, contact Roe Mobile Development, LLC at
